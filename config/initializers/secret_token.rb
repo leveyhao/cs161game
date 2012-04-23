ca9026cc2303e887ad44532df57d175555c259d1
Levey Hao <leveyhao@gmail.com>
2012-04-23 15:39:15 -0600

    initial commit with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cs161game::Application.config.secret_token = '0fc5d43fada19406e48cf543a93c3e8c893d67886103ed8e86d68b69e33bef8404e990cc4fd96b5e716cde7a774ab8c5fa40b28a7e366f7b2a65b97455ff4016'
