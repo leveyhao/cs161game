46b7c770a03630c11284bc9165c12d2d8e10c614
Levey Hao <leveyhao@gmail.com>
2012-05-05 17:41:41 -0600

    changed find(:all) to find_each to avoid heroku memory problems

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ActiveRecord::Base
       fields = [:user_id, :time, :event, :info1, :info2, :info3]
       CSV.open("public/events.csv", "wb") do |csv|
         csv << fields
-        Event.find(:all, :order => "user_id, time").each do |e|        
+        Event.find_each do |e|        
           csv << fields.map { |f| 
             if f == :time
               e.time.to_time.to_i * 1000 + e.time.usec / 1000
