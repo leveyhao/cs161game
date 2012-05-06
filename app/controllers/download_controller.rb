dc608dbfd2a756bbf711a482b29327dd3998c3d7
Levey Hao <leveyhao@gmail.com>
2012-05-06 01:35:21 -0600

    fixed typo for rails root

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,12 +1,12 @@
 class DownloadController < ApplicationController
    
    def download      
-    send_file("#{RAILS_ROOT}/tmp/events.csv", :type => "application/csv")    
+    send_file("#{Rails.root}/tmp/events.csv", :type => "application/csv")    
    end
 
    def userdata
     Event.generate_user_csv(params[:user])
-    send_file("#{RAILS_ROOT}/tmp/events.csv", :type => "application/csv")
+    send_file("#{Rails.root}/tmp/events.csv", :type => "application/csv")
    end
    
 end
