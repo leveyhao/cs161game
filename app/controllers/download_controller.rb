b82a03d2398875304dd1a78cac8905eb66deff6e
Levey Hao <leveyhao@gmail.com>
2012-05-06 01:27:57 -0600

    reattempt to make downloads work

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,12 +1,12 @@
 class DownloadController < ApplicationController
    
    def download      
-    send_file("#{Rails.root}/public/events.csv", :type => "application/csv")    
+    send_file("/public/events.csv", :type => "application/csv")    
    end
 
    def userdata
     Event.generate_user_csv(params[:user])
-    send_file("#{Rails.root}/public/events.csv", :type => "application/csv")
+    send_file("/public/events.csv", :type => "application/csv")
    end
    
 end
